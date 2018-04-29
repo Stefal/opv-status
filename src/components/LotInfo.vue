0ca100cb22e592a7b1a88665d799e19cdf85e997
Simon Archieri <simon@archieri.fr>
2018-04-29 20:43:01 +0200

    Add info to lot

---
+++ src/components/LotInfo.vue
@@ -0,0 +1,34 @@
+<template>
+   <div v-if="lot !== null">
+     <p>Lot : {{lot.id_lot}} | Date : {{lot.takenDate}} | Gps cord : {{lot.sensors.gps_pos.coordinates}} | <router-link v-if="lot.tile.id_tile !== null" :to="'/viewer/' + lot.tile.id_tile + '/' + lot.id_malette">See</router-link></p>
+     <img v-for="i in img" v-bind:key="i" v-bind:src="i" class="apn">
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'LotInfo',
+  data () {
+    return {
+      lot: null,
+      img: [],
+      imgNb: 6
+    }
+  },
+  methods: {
+    setLot (lot) {
+      this.lot = lot
+      this.img = []
+      for (var i = 0; i < this.imgNb; i++) {
+        this.img.push(('http://opv_master:5050/v1/files/' + lot.pictures_path + '/APN' + i + '.JPG'))
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.apn{
+  width: 150px;
+}
+</style>
