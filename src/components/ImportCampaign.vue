b1d2941aff99e019717827c719505a71eb049c48
Simon Archieri <simon@archieri.fr>
2018-05-12 17:46:32 +0200

    Revert "Accept exception from api"
    This reverts commit 0e7f9530f29c8f843d2fa533059f28f76f75aab6.

--- src/components/ImportCampaign.vue
+++ src/components/ImportCampaign.vue
@@ -2,13 +2,15 @@
   <div class='column'>
     <div style='width: 50%'>
       <h1>Import data</h1>
-      Campaign dir : <input ref='campaignDir'><br>
-      Campaign name : <input ref='campaignName'><br>
-      Id malette : <input ref='idMalette' value='1'><br>
-      Id rederbro : <input ref='idRederbro' value='1'><br>
-      Camera number : <input ref='cameraNB' value='6'><br>
-      Description : <input ref='description' value='An amazing campaign'><br>
+      Campaign dir : <input ref="campaignDir"><br>
+      Campaign name : <input ref="campaignName"><br>
+      Id malette : <input ref="idMalette" value="1"><br>
+      Id rederbro : <input ref="idRederbro" value="1"><br>
+      Camera number : <input ref="cameraNB" value="6"><br>
+      Description : <input ref="description" value="An amazing campaign"><br>
       <input type='button' value='Launch import' v-on:click='launch()'><br>
+      Log file : <input ref="logFile">
+      <input type='button' value='Show log' v-on:click='showLog()'><br>
       <input type='button' value='Scan for end' v-on:click='scan()'><br>
       Your campaign dir must have a tree like that : <br>
       campaign<br>
@@ -20,8 +22,8 @@
       Status : {{ status }}<br>
       Doing : {{ doing }}<br>
       Pourcent : {{ pourcent }}<br>
-      Time : {{time}}<br>
       Error : {{ error }}<br>
+      Log : {{ log }}<br>
     </div>
   </div>
 </template>
@@ -38,11 +40,24 @@ export default {
       doing: 'nothing',
       pourcent: 0,
       error: 0,
-      doScan: null,
-      time: 0
+      log: '',
+      doScan: null
     }
   },
   methods: {
+    showLog () {
+      var args = '{}'
+      if (this.$refs.logFile.value !== '') {
+        args = JSON.stringify({'logFile': this.$refs.logFile.value})
+      }
+      axios.post('http://opv_master:5001/import/log', args)
+        .then(answer => {
+          this.log = answer.data.answer
+        })
+        .catch(error => {
+          this.log = error.response.data.error
+        })
+    },
     launch () {
       var args = JSON.stringify({
         'path': this.$refs.campaignDir.value,
@@ -55,13 +70,13 @@ export default {
 
       axios.post('http://opv_master:5001/import/launch', args)
         .catch(error => {
+          console.log(error)
           this.error = error.response.data
         })
     },
     scan () {
       const that = this
       this.doScan = setInterval(function () {
-        that.status = 'up'
         fetch('http://opv_master:5001/import/status')
           .then(answer => answer.json())
           .then(json => {
@@ -70,7 +85,6 @@ export default {
             that.doing = json.doing
             that.pourcent = json.pourcent
             that.status = json.status
-            that.time = json.time
           })
       }, 1000)
     }
