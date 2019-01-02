10dffa410c9528bfe8fb0dfe18c352864351f118
Stefal <stephane.peneau@wanadoo.fr>
2019-01-02 12:37:50 +0100

    fix missing ()

--- src/components/campaign/CampaignInfo.vue
+++ src/components/campaign/CampaignInfo.vue
@@ -82,8 +82,8 @@ export default {
           var complet = false
           for (var picture in answer.data) {
             picture = answer.data[picture]
-            if (picture === 'APN' + (this.minPic - 1) + '.JPG') ||
-               (picture === 'APN' + (this.minPic - 1) + '.jpg')  {
+            if ((picture === 'APN' + (this.minPic - 1) + '.JPG') ||
+               (picture === 'APN' + (this.minPic - 1) + '.jpg'))  {
               complet = true
             }
           }
